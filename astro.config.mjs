6b356ea76bee954781ba0b7f0ac40f17f96e83b8
Jack Clackett <jack@noquarter.co>
2023-02-09 15:43:10 +0100

    remove adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config"
 import tailwind from "@astrojs/tailwind"
 import svelte from "@astrojs/svelte"
-import vercel from "@astrojs/vercel/edge"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), svelte()],
   output: "server",
-  adapter: vercel(),
 })

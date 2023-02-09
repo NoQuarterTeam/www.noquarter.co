c94e32f8b07b216e1fe3e3a56576e0b937037431
Jack Clackett <jack@noquarter.co>
2023-02-09 15:40:10 +0100

    test vercel adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import svelte from "@astrojs/svelte";
+import { defineConfig } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import svelte from "@astrojs/svelte"
+import vercel from "@astrojs/vercel/edge"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()]
-});
+  integrations: [tailwind(), svelte()],
+  output: "static",
+  adapter: vercel(),
+})

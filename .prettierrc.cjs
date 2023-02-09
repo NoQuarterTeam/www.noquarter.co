85a2334ac7214cfb2118d42eeaaf284941df4058
Jack Clackett <jack@noquarter.co>
2023-02-09 15:03:06 +0100

    fix: add prettier

---
+++ .prettierrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  semi: false,
+  singleQuote: false,
+  printWidth: 110,
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+}

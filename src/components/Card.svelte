34c2153a1c70c5e305904d93fc05ad9c411d20c4
Jack Clackett <jack@noquarter.co>
2023-02-13 13:15:04 +0100

    tweak bg

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -6,7 +6,7 @@
 </script>
 
 <div
-  class="p-8 space-y-3 bg-neutral-800 shadow-2xl border border-neutral-700 rounded-3xl"
+  class="p-8 space-y-3 bg-neutral-800 backdrop-blur-lg bg-gradient-to-bl from-purple-900/10 to-pink-900/5 shadow-2xl border border-neutral-700 rounded-3xl"
   use:tilt={{ scale: 1 }}
 >
   <p class="text-3xl">{title}</p>

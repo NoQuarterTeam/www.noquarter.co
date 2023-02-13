21f2545492d4d64e15338f8d189cd4b21a809cd5
Jack Clackett <jack@noquarter.co>
2023-02-13 12:22:27 +0100

    add animations

---
+++ src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { tilt } from "../lib/tilt"
+  export let title: string
+  export let desciption: string
+  export let tags: string[]
+</script>
+
+<div
+  class="p-8 space-y-3 bg-neutral-800 shadow-2xl border border-neutral-700 rounded-3xl"
+  use:tilt={{ scale: 1 }}
+>
+  <p class="text-3xl">{title}</p>
+  <p class="opacity-70">{desciption}</p>
+  <div class="flex space-x-2">
+    {#each tags as tag}
+      <span class="px-3 py-1 text-sm text-neutral-100 bg-neutral-700 rounded-xl">
+        {tag}
+      </span>
+    {/each}
+  </div>
+</div>
--- src/components/Counter.svelte
+++
@@ -1,7 +0,0 @@
-<script module="ts">
-  let count = 0
-</script>
-
-<button class="px-4 py-2 bg-green-500 rounded-sm hover:opacity-90 focus:opacity-80" on:click={() => count++}
-  >Count = {count}</button
->

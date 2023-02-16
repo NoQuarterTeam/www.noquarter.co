d8c7ef0ce8faaf12b82eccce551d88ff4ffb1216
Jack Clackett <jack@noquarter.co>
2023-02-16 12:53:31 +0100

    tap size

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -85,7 +85,10 @@
   {#if isLikeable}
     <button
       aria-label="like content"
-      class={join("absolute top-5 right-5 scale-0 transition-transform delay-200", isLoaded && "scale-100")}
+      class={join(
+        "absolute p-4 top-2 right-2 scale-0 delay-200 hover:scale-110 transition-transform opacity-80",
+        isLoaded && "scale-100",
+      )}
       on:click={toggleSave}
     >
       {#if isLiked}
@@ -94,7 +97,7 @@
           fill="currentColor"
           stroke-width="0"
           viewBox="0 0 512 512"
-          class="w-6 h-6 hover:scale-110 transition-transform opacity-80"
+          class="w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
           ><path
             d="M256 448l-30.164-27.211C118.718 322.442 48 258.61 48 179.095 48 114.221 97.918 64 162.4 64c36.399 0 70.717 16.742 93.6 43.947C278.882 80.742 313.199 64 349.6 64 414.082 64 464 114.221 464 179.095c0 79.516-70.719 143.348-177.836 241.694L256 448z"
@@ -106,7 +109,7 @@
           fill="currentColor"
           stroke-width="0"
           viewBox="0 0 512 512"
-          class="w-6 h-6 hover:scale-110 transition-transform opacity-80"
+          class="w-6 h-6"
           xmlns="http://www.w3.org/2000/svg"
           ><path
             d="M349.6 64c-36.4 0-70.7 16.7-93.6 43.9C233.1 80.7 198.8 64 162.4 64 97.9 64 48 114.2 48 179.1c0 79.5 70.7 143.3 177.8 241.7L256 448l30.2-27.2C393.3 322.4 464 258.6 464 179.1 464 114.2 414.1 64 349.6 64zm-80.8 329.3l-4.2 3.9-8.6 7.8-8.6-7.8-4.2-3.9c-50.4-46.3-94-86.3-122.7-122-28-34.7-40.4-63.1-40.4-92.2 0-22.9 8.4-43.9 23.7-59.3 15.2-15.4 36-23.8 58.6-23.8 26.1 0 52 12.2 69.1 32.5l24.5 29.1 24.5-29.1c17.1-20.4 43-32.5 69.1-32.5 22.6 0 43.4 8.4 58.7 23.8 15.3 15.4 23.7 36.5 23.7 59.3 0 29-12.5 57.5-40.4 92.2-28.8 35.7-72.3 75.7-122.8 122z"

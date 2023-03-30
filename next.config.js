57d587fe706644ceac522bd9dceda125b2063960
Jack Clackett <jack@noquarter.co>
2023-03-30 16:59:34 +0200

    image cache ttl

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    minimumCacheTTL: 60 * 60 * 24 * 60, // 7 days
+    minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
     remotePatterns: [
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "d23esfr6ddgb3k.cloudfront.net" },

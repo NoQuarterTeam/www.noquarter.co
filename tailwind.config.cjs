c4d7940e42a99895fffe4bf70f44a90994ab656b
Jack Clackett <jack@noquarter.co>
2023-04-14 11:23:11 +0200

    better custom font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,66 +13,8 @@ module.exports = {
         "brand-pink": "#F15BB5",
       },
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
-        serif: ["Poppins", ...defaultTheme.fontFamily.serif],
-      },
-      animation: {
-        sway: "sway 12s ease-in-out infinite",
-        sway2: "sway2 20s ease-in-out infinite",
-        sway3: "sway3 14s ease-in-out infinite",
-      },
-      keyframes: {
-        sway: {
-          "0%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": {
-            transform: "translate(200px, 200px) scale(1.2)",
-          },
-          "50%": {
-            transform: "translate(400px, 400px) scale(1.5)",
-          },
-          "66%": {
-            transform: "translate(200px, 200px) scale(1.2)",
-          },
-          "100%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-        },
-        sway2: {
-          "0%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": {
-            transform: "translate(-200px, 100px) scale(1.2)",
-          },
-          "50%": {
-            transform: "translate(-500px, 400px) scale(1.5)",
-          },
-          "66%": {
-            transform: "translate(-200px, 100px) scale(1.2)",
-          },
-          "100%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-        },
-        sway3: {
-          "0%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-          "33%": {
-            transform: "translate(200px, -200px) scale(1.2)",
-          },
-          "50%": {
-            transform: "translate(200px, -300px) scale(1.5)",
-          },
-          "66%": {
-            transform: "translate(200px, -200px) scale(1.2)",
-          },
-          "100%": {
-            transform: "translate(0px, 0px) scale(1)",
-          },
-        },
+        sans: ["var(--font-poppins)"],
+        serif: ["var(--font-poppins)"],
       },
     },
   },

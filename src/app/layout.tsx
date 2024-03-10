d6d329fe0f1371b74f16bd73e363a173a91fd578
Jack Clackett <jack@noquarter.co>
2024-03-10 11:04:47 +0100

    revert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,6 @@ export const metadata = {
   },
 }
 
-export const runtime = "edge"
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={`${karla.variable}`}>

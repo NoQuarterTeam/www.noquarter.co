348c0e0256b74fbe5e03e00020db05ca69db9a0e
Jack Clackett <jack@noquarter.co>
2024-10-03 16:40:27 +0200

    temp ts

--- src/app/v2/Player.tsx
+++ src/app/v2/Player.tsx
@@ -18,28 +18,35 @@ export function Player() {
     const { forward, backward, left, right, jump } = get()
     if (!playerRef.current) return
 
+    // @ts-ignore
     const velocity = playerRef.current.linvel()
 
     // update camera
+    // @ts-ignore
     const position = playerRef.current.translation()
     state.camera.position.set(position.x, position.y, position.z)
 
     // movement
+    // @ts-ignore
     frontVector.set(0, 0, backward - forward)
+    // @ts-ignore
     sideVector.set(left - right, 0, 0)
     direction.subVectors(frontVector, sideVector).normalize().multiplyScalar(SPEED).applyEuler(state.camera.rotation)
+    // @ts-ignore
     playerRef.current.setLinvel({ x: direction.x, y: velocity.y, z: direction.z })
     // // jumping
     // const world = rapier.world.raw()
 
     // const ray = world.castRay(new RAPIER.Ray(playerRef.current.translation(), { x: 0, y: -1, z: 0 }))
     // const grounded = ray && ray.collider && Math.abs(ray.toi) <= 1.75
+    // @ts-ignore
     if (jump) playerRef.current.setLinvel({ x: 0, y: 7.5, z: 0 })
   })
 
   return (
     <>
       <RigidBody
+        // @ts-ignore
         ref={playerRef}
         colliders={false}
         mass={1}
